ddaee6944f6a78fb1a3b02d47e0adcfe3d78220c
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-17 10:15:57 -0500

    backend starting point

---
+++ index.tsx
@@ -0,0 +1,9 @@
+import client from "./prisma/client"; // importing the prisma instance we created.
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
